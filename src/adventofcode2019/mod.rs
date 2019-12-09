9a0b76f3978a41a26f72223d4fdf8e17561872e0
Alon Yehezkel <alony@bookaway.com>
2019-12-09 13:59:52 +0200

    day 5

--- src/adventofcode2019/mod.rs
+++ src/adventofcode2019/mod.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 pub fn run() {
   println!("day1-1: {}", day1::get_part1());
@@ -12,4 +13,6 @@ pub fn run() {
   println!("day3-2: {}", day3::get_part2());
   println!("day4-1: {}", day4::get_part1());
   println!("day4-2: {}", day4::get_part2());
+  println!("day5-1: {}", day5::get_part1());
+  println!("day5-2: {}", day5::get_part2());
 }
